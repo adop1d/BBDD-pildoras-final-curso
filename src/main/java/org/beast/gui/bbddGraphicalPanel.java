89689e97e4d732f32e82fb48400e6bfb60485523
Kelvin Puch <kelpucha@gmail.com>
2024-11-26 17:54:59 -0400

    added a connection to database, just for this time, ill change it later

--- src/main/java/org/beast/gui/bbddGraphicalPanel.java
+++ src/main/java/org/beast/gui/bbddGraphicalPanel.java
@@ -2,6 +2,8 @@ package org.beast.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class bbddGraphicalPanel extends JPanel {
     public bbddGraphicalPanel(){
@@ -10,8 +12,21 @@ public class bbddGraphicalPanel extends JPanel {
         resourceInfo=new JTextArea();
         add(resourceFinder, BorderLayout.NORTH);
         add(resourceInfo,BorderLayout.CENTER);
+    }
+    public void wireToDataBase(){
+
+        coneccion=null;
+
+        try{
+            coneccion= DriverManager.getConnection("jdbc:postgresql://localhost:9292/demo","postgres","12345678");
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
     }
+
     private JComboBox resourceFinder;
     private JTextArea resourceInfo;
+    private Connection coneccion;
 }
