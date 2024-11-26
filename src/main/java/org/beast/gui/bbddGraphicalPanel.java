be0fbc85d8b9a7773732f1378efd3d1e67b2d342
Kelvin Puch <kelpucha@gmail.com>
2024-11-26 18:45:44 -0400

    added showInfoText method, to run the tables of the database

--- src/main/java/org/beast/gui/bbddGraphicalPanel.java
+++ src/main/java/org/beast/gui/bbddGraphicalPanel.java
@@ -2,10 +2,10 @@ package org.beast.gui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class bbddGraphicalPanel extends JPanel {
     public bbddGraphicalPanel(){
@@ -14,11 +14,18 @@ public class bbddGraphicalPanel extends JPanel {
         resourceFinder=new JComboBox();
         resourceInfo=new JTextArea();
 
-        add(resourceFinder, BorderLayout.NORTH);
-        add(resourceInfo,BorderLayout.CENTER);
-
         wireToDataBase();
         getTables();
+
+        resourceFinder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String tableName = (String)resourceFinder.getSelectedItem();
+                showInfoText(tableName);
+            }
+        });
+        add(resourceFinder, BorderLayout.NORTH);
+        add(resourceInfo,BorderLayout.CENTER);
     }
     public void wireToDataBase(){
         coneccion=null;
@@ -35,14 +42,40 @@ public class bbddGraphicalPanel extends JPanel {
             myResultSet=dataDDBB.getTables("demo",null,null,null);
 
             while(myResultSet.next()){
-
-                resourceFinder.addItem(myResultSet.getString("TABLE NAME"));
-
+                resourceFinder.addItem(myResultSet.getString("TABLE_NAME"));
             }
 
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
+    }
+    public void showInfoText(String table){
+
+        ArrayList<String> fields=new ArrayList<>();
+        String consultation="SELECT * FROM " + table;
+
+        try{
+            resourceInfo.setText("");
+
+            Statement statement=coneccion.createStatement();
+            ResultSet myResultSet=statement.executeQuery(consultation);
+            ResultSetMetaData resultSetDataBase=myResultSet.getMetaData();
+
+            for (int i = 1; i <=resultSetDataBase.getColumnCount(); i++) {
+
+                fields.add(resultSetDataBase.getColumnLabel(i));
+            }
+            while (myResultSet.next()){
+                for(String fieldName:fields){
+                    resourceInfo.append(myResultSet.getString(fieldName)+" ");
+                }
+                resourceInfo.append("\n");
+            }
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 
     private JComboBox resourceFinder;
